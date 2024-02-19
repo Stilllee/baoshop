49b2262ea2390b5c4360689528f8f6b90a2430b6
Stilllee <orodae@gmail.com>
2024-02-19 18:48:55 +0900

    ✨ 로그아웃 구현

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,10 +1,17 @@
-import React from "react";
+import React, { useState } from "react";
 import { Link } from "react-router-dom";
 import { SiFoodpanda } from "react-icons/si";
 import { FaPencilAlt } from "react-icons/fa";
-import { login } from "../api/firebase";
+import { login, logout } from "../api/firebase";
 
 export default function Navbar() {
+  const [user, setUser] = useState();
+  const handleLogin = () => {
+    login().then(setUser);
+  };
+  const handleLogout = () => {
+    logout().then(setUser);
+  };
   return (
     <header className="flex justify-between border-b border-black p-5">
       <Link to="/" className="flex items-center text-3xl gap-2 font-semibold">
@@ -17,7 +24,8 @@ export default function Navbar() {
         <Link to="/products/new">
           <FaPencilAlt />
         </Link>
-        <button onClick={login}>Login</button>
+        {!user && <button onClick={handleLogin}>Login</button>}
+        {user && <button onClick={handleLogout}>Logout</button>}
       </nav>
     </header>
   );
