bc7ce6eb98cb4cfa32f0b2b03ea81ab70c742e48
Stilllee <orodae@gmail.com>
2024-02-19 18:49:54 +0900

    ✨ 사용자 인증 상태 관리 로직 업데이트

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,11 +1,18 @@
-import React, { useState } from "react";
+import React, { useEffect, useState } from "react";
 import { Link } from "react-router-dom";
 import { SiFoodpanda } from "react-icons/si";
 import { FaPencilAlt } from "react-icons/fa";
-import { login, logout } from "../api/firebase";
+import { login, logout, onUserStateChange } from "../api/firebase";
 
 export default function Navbar() {
   const [user, setUser] = useState();
+
+  useEffect(() => {
+    onUserStateChange((user) => {
+      console.log(user);
+      setUser(user);
+    });
+  });
   const handleLogin = () => {
     login().then(setUser);
   };
