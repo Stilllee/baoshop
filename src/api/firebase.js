f4a3a3b74e702efcab7957043763ef11fe92b283
Stilllee <orodae@gmail.com>
2024-02-19 18:19:35 +0900

    🔧 firebase 설정 추가

---
+++ src/api/firebase.js
@@ -0,0 +1,9 @@
+import { initializeApp } from "firebase/app";
+const firebaseConfig = {
+  apiKey: process.env.REACT_APP_FIREBASE,
+  authDomain: REACT_APP_FIREBASE_AUTH_DOMAIN,
+  databaseURL: REACT_APP_FIREBASE_DB_URL,
+  projectId: REACT_APP_FIREBASE_PROJECT_ID,
+};
+
+const app = initializeApp(firebaseConfig);
