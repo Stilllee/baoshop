603665f98449e686fdb06d7feab718230a0297c6
Stilllee <orodae@gmail.com>
2024-02-22 23:06:10 +0900

    💄 배너 스타일 수정

--- src/components/Banner.jsx
+++ src/components/Banner.jsx
@@ -4,9 +4,9 @@ export default function Banner() {
   return (
     <section className="relative h-160 bg-mint">
       <div className="w-full h-full bg-bottom bg-banner opacity-80" />
-      <div className="absolute w-full text-center text-white top-64">
-        <h2 className="mb-1 text-5xl">We Are</h2>
-        <p className="text-3xl">Bao Familly</p>
+      <div className="absolute w-full font-extrabold text-center text-white uppercase opacity-65 top-64">
+        <h2 className="mb-2 text-5xl">We Are</h2>
+        <p className="text-5xl">Bao Familly</p>
       </div>
     </section>
   );
