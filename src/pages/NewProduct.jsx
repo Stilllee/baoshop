5a4a9dad6c858b3fbf0cffd07ebe09adba6915fc
Stilllee <orodae@gmail.com>
2024-02-25 23:51:44 +0900

    ✨ 새 제품 등록 기능 최적화
    - ReactQuery의 useMutation과 useQueryClient 사용
    - API 호출을 효율적으로 관리하고 제품 목록 캐시 자동 갱신 구현

--- src/pages/NewProduct.jsx
+++ src/pages/NewProduct.jsx
@@ -2,6 +2,7 @@ import React, { useState } from "react";
 import { uploadImage } from "../api/uploader";
 import Button from "../components/ui/Button";
 import { addNewProduct } from "../api/firebase";
+import { useMutation, useQueryClient } from "@tanstack/react-query";
 
 export default function NewProduct() {
   const [product, setProduct] = useState({});
@@ -9,18 +10,28 @@ export default function NewProduct() {
   const [isUploading, setIsUploading] = useState(false);
   const [success, setSuccess] = useState();
 
+  const queryClient = useQueryClient();
+  const addProduct = useMutation({
+    mutationFn: ({ product, url }) => addNewProduct(product, url),
+    onSuccess: () => queryClient.invalidateQueries(["products"]),
+  });
+
   const handleSubmit = (e) => {
     e.preventDefault();
     setIsUploading(true);
     uploadImage(file) //
       .then((url) => {
-        addNewProduct(product, url) //
-          .then(() => {
-            setSuccess("제품이 추가되었습니다.");
-            setTimeout(() => {
-              setSuccess(null);
-            }, 4000);
-          });
+        addProduct.mutate(
+          { product, url },
+          {
+            onSuccess: () => {
+              setSuccess("제품이 추가되었습니다.");
+              setTimeout(() => {
+                setSuccess(null);
+              }, 4000);
+            },
+          }
+        );
       })
       .finally(() => setIsUploading(false));
   };
