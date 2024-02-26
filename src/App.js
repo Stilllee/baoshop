7d402d4ab9b074cd736725a7f3b56a819273e226
Stilllee <orodae@gmail.com>
2024-02-26 15:46:28 +0900

    🐛 페이지 이동시 스크롤 위치가 상단으로 가게 코드 수정
    - 스크롤위치 복원되는 현상 제거

--- src/App.js
+++ src/App.js
@@ -1,10 +1,16 @@
-import { Outlet } from "react-router-dom";
+import { Outlet, useLocation } from "react-router-dom";
 import Navbar from "./components/Navbar";
 import { AuthContextProvider } from "./context/AuthContext";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useEffect } from "react";
 
 const queryClient = new QueryClient();
 function App() {
+  const location = useLocation();
+
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [location]);
   return (
     <QueryClientProvider client={queryClient}>
       <AuthContextProvider>
